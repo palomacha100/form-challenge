be85f4db42e0201e17986cc473d09fc395b46a31
palomacha100 <palomacha100@gmail.com>
2022-06-02 13:10:15 -0300

    Feat: Update button loggic

--- scripts/button.js
+++ scripts/button.js
@@ -1,3 +1,9 @@
-function myFunction() {
-    document.querySelector('.main-success').style.display = 'block';
+function success() {
+    document.querySelector('.main-success').style.display = 'flex';
+    document.querySelector('.main').style.display = 'none';
+}
+
+function goBack(){
+    document.querySelector('.main').style.display = 'flex';
+    document.querySelector('.main-success').style.display = 'none';
 }
